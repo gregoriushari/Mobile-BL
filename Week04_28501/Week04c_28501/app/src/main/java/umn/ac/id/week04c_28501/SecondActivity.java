6af047a796b9fdfc0917d1b8b522c5d5a2baaa8e
Ghs267 <hariyantosetiadi@gmail.com>
2021-02-24 16:00:52 +0700

    revisi bagian c

---
+++ Week04_28501/Week04c_28501/app/src/main/java/umn/ac/id/week04c_28501/SecondActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.week04c_28501;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
